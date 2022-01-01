15ba81e5cb2ffc29a8c20a481154ccf121750d1f
enikebraimoh <richardbraimoh@gmail.com>
2022-01-01 22:29:22 +0100

    started compose

---
+++ Compose_1/app/src/main/java/com/enike/compose_1/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.enike.compose_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.enike.compose_1.ui.theme.Compose_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    messageCard(Message("enike", "Jetpack Compose"))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun messageCard(name: Message) {
+    Row(modifier = Modifier.padding(all = 10.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "this is a profile image",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(40.dp)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column() {
+            Text(
+                text = name.user,
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.secondaryVariant
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = name.Message,
+                    Modifier.padding(all = 5.dp),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMessageCard() {
+    Compose_1Theme {
+        messageCard(Message("Enike", "Jetpack Compose"))
+    }
+}
---
+++ Compose_1/app/src/main/java/com/enike/compose_1/Message.kt
@@ -0,0 +1,3 @@
+package com.enike.compose_1
+
+data class Message(val user : String, val Message : String)
