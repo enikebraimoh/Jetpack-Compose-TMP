9cf5f35cbece9bb71e333cf456af9d04c2072b39
enikebraimoh <richardbraimoh@gmail.com>
2022-01-03 23:03:41 +0100

    added onboarding screen ✨

--- compose_2/app/src/main/java/com/adashi/compose_2/MainActivity.kt
+++ compose_2/app/src/main/java/com/adashi/compose_2/MainActivity.kt
@@ -3,15 +3,10 @@ package com.adashi.compose_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +27,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun App(names: List<String> = listOf("World", "Compose")) {
+fun ShowOnBoardingScreen() {
+
+    //var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                onClick = {  },
+                modifier = Modifier.padding(all = 24.dp)
+            ) {
+                Text(text = "Contunue")
+
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun App() {
+    Greet()
+}
+
+@Composable
+fun Greet(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -42,7 +67,10 @@ fun App(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember{ mutableStateOf(false) }
+    val expanded = remember { mutableStateOf(false) }
+
+    val extrapadding = if (expanded.value) 50.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -52,7 +80,11 @@ fun Greeting(name: String) {
                 .fillMaxWidth()
                 .padding(24.dp)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extrapadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
@@ -68,5 +100,13 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     Compose_2Theme {
         App()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewOnBoarding() {
+    Compose_2Theme {
+        ShowOnBoardingScreen()
     }
 }
