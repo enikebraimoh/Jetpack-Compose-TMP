4670cc58a22773875117e3fc2239fc2de7880e3c
enikebraimoh <richardbraimoh@gmail.com>
2022-01-05 08:31:45 +0100

    added app bar

--- compose_3/app/src/main/java/com/enike/compose_3/MainActivity.kt
+++ compose_3/app/src/main/java/com/enike/compose_3/MainActivity.kt
@@ -3,11 +3,25 @@ package com.enike.compose_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.enike.compose_3.ui.theme.Compose_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +31,7 @@ class MainActivity : ComponentActivity() {
             Compose_3Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotoGraphyCard()
                 }
             }
         }
@@ -25,14 +39,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title =  {
+                    Text(text = "My Tool Bar")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Favorite,
+                            contentDescription = "just a vav icon")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        ContentBody(Modifier.padding(innerPadding))
+    }
 }
+
+@Composable
+fun ContentBody(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hello Enike!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
+fun layoutPreview() {
+    Compose_3Theme {
+        LayoutsCodelab()
+    }
+}
+
+@Composable
+fun PhotoGraphyCard() {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring onClick */ })
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
+
+}
+
+
+@Composable
 fun DefaultPreview() {
     Compose_3Theme {
-        Greeting("Android")
+        PhotoGraphyCard()
     }
 }
