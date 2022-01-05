352cc05a50d186e081d37a7dc88d831d88f7bc03
enikebraimoh <richardbraimoh@gmail.com>
2022-01-05 18:28:14 +0100

    I created my own custom Modifier and created my own custom Layout 💃🏾💃🏾

--- compose_3/app/src/main/java/com/enike/compose_3/MainActivity.kt
+++ compose_3/app/src/main/java/com/enike/compose_3/MainActivity.kt
@@ -3,12 +3,12 @@ package com.enike.compose_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,13 +16,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.enike.compose_3.ui.theme.Compose_3Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +39,7 @@ class MainActivity : ComponentActivity() {
             Compose_3Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PhotoGraphyCard()
+                    SimpleList()
                 }
             }
         }
@@ -43,13 +51,15 @@ fun LayoutsCodelab() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title =  {
+                title = {
                     Text(text = "My Tool Bar")
                 },
                 actions = {
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(imageVector = Icons.Filled.Favorite,
-                            contentDescription = "just a vav icon")
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "just a vav icon"
+                        )
                     }
                 }
             )
@@ -61,18 +71,56 @@ fun LayoutsCodelab() {
 
 @Composable
 fun ContentBody(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    MyOwnColumn(modifier = modifier) {
         Text(text = "Hello Enike!")
         Text(text = "Thanks for going through the Layouts codelab")
     }
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun layoutPreview() {
-    Compose_3Theme {
-        LayoutsCodelab()
+fun SimpleList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    Column() {
+        Row() {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                },
+            ) {
+                Text(text = "Scroll to Top")
+            }
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                },
+            ) {
+                Text(text = "Scroll to Bottom")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "", Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item $index", style = MaterialTheme.typography.subtitle1)
+
     }
 }
 
@@ -110,9 +158,81 @@ fun PhotoGraphyCard() {
 }
 
 
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // remove the baseline from the passing specified
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        var placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        var height = placeable.height + placeableY
+
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    })
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutPreview() {
+    Compose_3Theme {
+        LayoutsCodelab()
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Compose_3Theme {
         PhotoGraphyCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListPreview() {
+    Compose_3Theme {
+        SimpleList()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    Compose_3Theme() {
+        Text("Hi there!", modifier = Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Compose_3Theme() {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // measure and position children given constraints logic here
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        var yPosition = 0
+        layout(constraints.maxWidth,constraints.maxHeight){
+            placeables.forEach { placeable ->
+                placeable.placeRelative(0,yPosition)
+                yPosition += placeable.height
+            }
+        }
     }
 }
