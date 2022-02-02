59f7c857bae29e60271fc39ecc0f426323c9a204
enikebraimoh <richardbraimoh@gmail.com>
2022-02-02 10:01:07 +0100

    passinng arguements 🤩

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -22,13 +22,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -48,23 +59,49 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = RallyScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
-                    }
-                )
-            }
+           NavHost(
+               navController = navController,
+               startDestination = RallyScreen.Overview.name,
+               modifier = Modifier.padding(innerPadding)){
+
+               val accountsName = RallyScreen.Accounts.name
+               composable(route = "$accountsName/{name}",
+                   arguments = listOf(
+                       navArgument("name"){
+                           type = NavType.StringType
+                       }
+                   )){
+
+               }
+
+               composable(RallyScreen.Overview.name){
+                   OverviewBody(
+                       onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+                       onClickSeeAllBills = {navController.navigate(RallyScreen.Bills.name)}
+                   )
+               }
+               composable(RallyScreen.Accounts.name){
+                   AccountsBody(accounts = UserData.accounts)
+               }
+               composable(RallyScreen.Bills.name){
+                   BillsBody(bills = UserData.bills)
+               }
+           }
         }
     }
 }
