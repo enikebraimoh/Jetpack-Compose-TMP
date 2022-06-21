ac18b40fe6b8db27028c5fbf7e50baa73cfc2cae
enikebraimoh <richardbraimoh@gmail.com>
2022-06-21 03:12:18 +0100

    played with gestures

---
+++ Playground/app/src/main/java/com/enike/playground/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.enike.playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.enike.playground.ui.theme.PlaygroundTheme
+import kotlin.math.roundToInt
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Gesture()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Gesture() {
+    val offset = remember { Animatable(Offset(0f, 0f), Offset.VectorConverter) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                coroutineScope {
+                    while (true) {
+                        // Detect a tap event and obtain its position.
+                        val position = awaitPointerEventScope {
+                            awaitFirstDown().position
+                        }
+                        launch {
+                            // Animate to the tap position.
+                            offset.animateTo(position)
+                        }
+                    }
+                }
+            }
+    ) {
+        Surface(modifier = Modifier.offset { offset.value.toIntOffset() }.size(50.dp), color = Color.Gray, shape = RoundedCornerShape(20.dp)){}
+    }
+}
+
+private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
+
+
+@Composable
+fun Greeting(name: String) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PlaygroundTheme {
+        Greeting("Android")
+    }
+}
